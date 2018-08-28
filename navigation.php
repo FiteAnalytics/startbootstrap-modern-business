1cf90315c89c77d8018e9e88e20ea017822efa31
Geoff Fite <geoff@fiteanalytics.com>
2018-08-28 09:39:51 -0700

    gish-0001

--- navigation.php
+++ navigation.php
@@ -27,7 +27,7 @@
           </div>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="company.html">Company</a>
+          <a class="nav-link" href="about.php">Company</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="contact.html">Contact</a>
