292d4c5288600275b1c7ef10e2d67214911e2c54
Geoff Fite <geoff@fiteanalytics.com>
2018-08-28 08:58:59 -0700

    gish-0001

--- navigation.php
+++ navigation.php
@@ -1,2 +1,45 @@
-<?php echo '<p>Hello World</p>'; ?>
-<?php phpinfo(); ?>
+<!-- Navigation -->
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="index.html">&gt; fite analytics</a>
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="index.html">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="asset_managers.html">for Asset Managers</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="broker_dealers.html">for Broker-Dealers</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Products & Services
+          </a>
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+            <a class="dropdown-item" href="portfolio-1-col.html">CMS:Portfolio</a>
+            <a class="dropdown-item" href="portfolio-2-col.html">Data Management Services</a>
+            <a class="dropdown-item" href="portfolio-3-col.html">Feeds & APIs</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="company.html">Company</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contact.html">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="navbar under-nav">
+  <h1 class="hero-text">CMS:Portfolio
+    <small>Fixed Income Analytics</small>
+  </h1>
+  <h2 class="hero-subtext">Accurate, Comprehensive, & Extremely Fast</h2>
+</div>
