9369067bf482de195e4e51c340d787bad5a84594
Geoff Fite <geoff@fiteanalytics.com>
2018-08-30 16:16:09 -0700

    gish-0001

--- about.php
+++ about.php
@@ -113,7 +113,41 @@
     </div>
     <!-- /.container -->
     <?php include 'footer.php'; ?>
-    <?php include 'google_tracker.php';>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-44140025-1', 'auto');
+      ga('require', 'displayfeatures');
+      ga('send', 'pageview');
+    </script>
+    <script type='application/ld+json'>
+    {
+      "@context": "http://www.schema.org",
+      "@type": "Corporation",
+      "name": "Fite Analytics LLC",
+      "founder": "Geoff Fite",
+      "url": "www.fiteanalytics.com",
+      "sameAs": [
+        "fiteanalytics.com"
+      ],
+      "description": "financial software and data",
+      "address": {
+        "@type": "PostalAddress",
+        "postOfficeBoxNumber": "",
+        "addressLocality": "MANHATTAN BEACH",
+        "addressRegion": "CA",
+        "postalCode": "90266-2558",
+        "addressCountry": "USA"
+      },
+      "contactPoint": {
+        "@type": "ContactPoint",
+        "telephone": "+14242620068",
+        "contactType": "customer support"
+      }
+    }
+    </script>
   </body>
 
 </html>
