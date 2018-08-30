61fe156bdd1adfbaf26d4657250a2cac8081e6b1
Geoff Fite <geoff@fiteanalytics.com>
2018-08-29 22:04:42 -0700

    gish-0001

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <footer class="py-5 bg-dark">
       <div class="container">
         <p class="m-0 text-center text-white">Copyright &copy; Fite Analytics LLC 2018</p>
-        <a href="tos.php">site terms</a>
+        <a class="m-0 text-center text-white" href="tos.php">site terms</a>
       </div>
       <!-- /.container -->
     </footer>
