362206d64efcc4855d3bbc7bbbbcea47a6779f48
Geoff Fite <geoff@fiteanalytics.com>
2018-08-29 20:40:49 -0700

    gish-0001

--- contact.php
+++ contact.php
@@ -29,31 +29,25 @@
         </div>
     </div>
     <br>
+    <div class="container">
       <!-- Content Row -->
       <div class="row">
         <!-- Map Column -->
         <div class="col-lg-8 mb-4">
           <!-- Embedded Google Map -->
-          <iframe width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;ll=37.0625,-95.677068&amp;spn=56.506174,79.013672&amp;t=m&amp;z=4&amp;output=embed"></iframe>
+          <iframe width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/place/1240+Rosecrans+Ave,+Manhattan+Beach,+CA+90266/@33.9009564,-118.3946283,17z/data=!3m1!4b1!4m5!3m4!1s0x80c2b4076f63cd35:0x61cd12182890d14!8m2!3d33.9009564!4d-118.3924396"></iframe>
         </div>
         <!-- Contact Details Column -->
         <div class="col-lg-4 mb-4">
           <h3>Contact Details</h3>
           <p>
-            3481 Melrose Place
-            <br>Beverly Hills, CA 90210
+            1240 Rosecrans Ave.
+            <br>Suite 120
+            <br>Manhattan Beach, CA 90266
             <br>
           </p>
           <p>
-            <abbr title="Phone">P</abbr>: (123) 456-7890
-          </p>
-          <p>
-            <abbr title="Email">E</abbr>:
-            <a href="mailto:name@example.com">name@example.com
-            </a>
-          </p>
-          <p>
-            <abbr title="Hours">H</abbr>: Monday - Friday: 9:00 AM to 5:00 PM
+            <abbr title="Phone">P</abbr>: (414) 262-0068
           </p>
         </div>
       </div>
@@ -105,7 +99,7 @@
     <!-- Footer -->
     <footer class="py-5 bg-dark">
       <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2018</p>
+        <p class="m-0 text-center text-white">Copyright &copy; Fite Analytics LLC 2018</p>
       </div>
       <!-- /.container -->
     </footer>
