be42a2e3970e4ffd1f0880a14e5e870f65a293c3
Geoff Fite <geoff@fiteanalytics.com>
2018-08-29 21:44:55 -0700

    gish-0001

--- data_management.php
+++ data_management.php
@@ -22,9 +22,6 @@
     <?php include 'navigation.php'; ?>
     <div class="navbar under-nav">
         <div class="container centered-text">
-          <h1 class="hero-text">CMS:Portfolio
-            <small>Fixed-Income Analytics</small>
-          </h1>
           <h2 class="hero-subtext">Data Management Services</h2>
         </div>
     </div>
