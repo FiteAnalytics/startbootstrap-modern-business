239e7ac42e31facb40f1eae3425aa84bba5c1ed4
Geoff Fite <geoff@fiteanalytics.com>
2018-08-30 07:05:10 -0700

    gish-0001

--- asset_managers.php
+++ asset_managers.php
@@ -33,15 +33,15 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-6">
-          <img class="img-fluid rounded" src="https://s3-us-west-1.amazonaws.com/fiteanalytics/website-assets/shot_benchmark_yield.png" alt="">
-        </div>
-        <div class="col-lg-6">
           <h2 class="whitetext">how we can help</h2>
           <p class="justify-text">Understand Risk & Return at the portfolio, asset-class, and security level, versus benchmarks or liabilities.
           Fully explain historical risk and performance with CMS:Portfolio, an industry-leading portfolio analytics platform
           designed to perform what-if analyses, conduct stress tests and yield curve scenarios, and shock all drivers of risk
           and return over different horizons.
           </p>
+        </div>
+        <div class="col-lg-6">
+          <img class="img-fluid rounded" src="https://s3-us-west-1.amazonaws.com/fiteanalytics/website-assets/shot_benchmark_yield.png" alt="">
         </div>
       </div>
       <!-- /.row -->
