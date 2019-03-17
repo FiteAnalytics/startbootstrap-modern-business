b8cf65e8601d94c85d6da4684560ea49576ffcd0
Geoff Fite <geoff@fiteanalytics.com>
2019-03-17 13:56:36 -0700

    gish-0001

--- index2.php
+++ index2.php
@@ -111,105 +111,103 @@
                 </div>
             </div>
         </div>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <h1 class="hero-text" style="margin: 0; text-align: right">CMS:Portfolio <small>Fixed-Income Analytics Platform</small></h1>
-          </div>
+        <br><hr><br>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="hero-text" style="margin: 0; text-align: left">CMS:Portfolio <small>Fixed-Income Analytics Platform</small></h1>
+                </div>
+            </div>
         </div>
-      </div>
-    <br>
-    <!-- Who We Are Section -->
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6">
-          <img class="img-fluid rounded" src="https://s3-us-west-1.amazonaws.com/fiteanalytics/website-assets/cms_diagram.png" alt="">
+        <br>
+        <!-- CMS:Portfolio Section -->
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6">
+                    <img class="img-fluid rounded" src="https://s3-us-west-1.amazonaws.com/fiteanalytics/website-assets/cms_diagram.png" alt="">
+                </div>
+                <div class="col-lg-6">
+                    <h2 class="whitetext">CMS:Portfolio</h2>
+                    <p class="justify-text">Fite Analytics helps fixed income investors manage Risk and Return. Our platform, <strong>CMS:Portfolio</strong>, is powered
+                        by benchmarks from <a class="subtle-a" href="https://us.spindices.com/" target="_blank">S&P Dow Jones Indices</a>
+                        and extensive market content
+                        from <a class="subtle-a" href="https://www.exchange-data.com/" target="_blank">Exchange Data International</a>.
+                        Cloud-native with zero-implementation,
+                        CMS:Portfolio sets the standard for <strong>fixed income portfolio and security analytics</strong>. We are
+                        industry veterans with over 70 years of combined fixed income analytics experience, and we are fanatical
+                        about having the happiest customers on the planet.</p>
+                </div>
+            </div>
+            <!-- /.row -->
+            <br><hr><br>
+            <!-- Call to Action Section -->
+            <div class="row mb-4">
+                <div class="col-md-8">
+                    <h6 class="peachtext">Do you have a portfolio that you would like to analyze on CMS:Portfolio? We can get you up and running quickly, usually the same day. We are serious about Zero Implementation!</h6>
+                </div>
+                <div class="col-md-4">
+                    <a class="btn btn-lg btn-secondary btn-block" href="contact.php">Contact Us Today</a>
+                </div>
+            </div>
+            <hr><br>
+            <!-- Features Section -->
+            <div class="row">
+                <div class="col-lg-9">
+                    <h2>CMS:Portfolio is built on the latest technology</h2>
+                    <ul>
+                        <li>Cloud-native on Amazon AWS</li>
+                        <li>Web applications, mobile-ready</li>
+                        <li>Distributed ledgers for security & interoperability</li>
+                        <li>Ultra-fast scalable computation grid</li>
+                        <li>Portfolio time-machine - view anything as-of any date</li>
+                        <li>Massively large portfolios - maintain any number of portfolios with any number of securities</li>
+                    </ul>
+                </div>
+                <div class="col-lg-3">
+                    <img class="img-fluid rounded" src="https://s3-us-west-1.amazonaws.com/fiteanalytics/website-assets/apn_logo.png" alt="">
+                    <img class="img-fluid rounded" src="https://s3-us-west-1.amazonaws.com/fiteanalytics/website-assets/rabbitmq.png" alt="">
+                    <img class="img-fluid rounded" src="https://s3-us-west-1.amazonaws.com/fiteanalytics/website-assets/hyperledger.png" alt="">
+                    <img class="img-fluid rounded" style="width: 100%;" src="https://s3-us-west-1.amazonaws.com/fiteanalytics/website-assets/python.png" alt="">
+                </div>
+            </div>
+            <!-- /.row -->
         </div>
-        <div class="col-lg-6">
-          <h2 class="whitetext">who we are</h2>
-          <p class="justify-text">Fite Analytics helps fixed income investors manage Risk and Return. Our platform, <strong>CMS:Portfolio</strong>, is powered
-            by benchmarks from <a class="subtle-a" href="https://us.spindices.com/" target="_blank">S&P Dow Jones Indices</a>
-            and extensive market content
-            from <a class="subtle-a" href="https://www.exchange-data.com/" target="_blank">Exchange Data International</a>.
-            Cloud-native with zero-implementation,
-            CMS:Portfolio sets the standard for <strong>fixed income portfolio and security analytics</strong>. We are
-            industry veterans with over 70 years of combined fixed income analytics experience, and we are fanatical
-            about having the happiest customers on the planet.</p>
-        </div>
-      </div>
-      <!-- /.row -->
-      <br>
-      <hr>
-      <br>
-      <!-- Call to Action Section -->
-      <div class="row mb-4">
-        <div class="col-md-8">
-          <h6 class="peachtext">Do you have a portfolio that you would like to analyze on CMS:Portfolio? We can get you up and running quickly, usually the same day. We are serious about Zero Implementation!</h6>
-        </div>
-        <div class="col-md-4">
-          <a class="btn btn-lg btn-secondary btn-block" href="contact.php">Contact Us Today</a>
-        </div>
-      </div>
-      <hr>
-      <br>
-      <!-- Features Section -->
-      <div class="row">
-        <div class="col-lg-9">
-          <h2>CMS:Portfolio is built on the latest technology</h2>
-          <ul>
-            <li>Cloud-native on Amazon AWS</li>
-            <li>Web applications, mobile-ready</li>
-            <li>Distributed ledgers for security & interoperability</li>
-            <li>Ultra-fast scalable computation grid</li>
-            <li>Portfolio time-machine - view anything as-of any date</li>
-            <li>Massively large portfolios - maintain any number of portfolios with any number of securities</li>
-          </ul>
-        </div>
-        <div class="col-lg-3">
-          <img class="img-fluid rounded" src="https://s3-us-west-1.amazonaws.com/fiteanalytics/website-assets/apn_logo.png" alt="">
-          <img class="img-fluid rounded" src="https://s3-us-west-1.amazonaws.com/fiteanalytics/website-assets/rabbitmq.png" alt="">
-          <img class="img-fluid rounded" src="https://s3-us-west-1.amazonaws.com/fiteanalytics/website-assets/hyperledger.png" alt="">
-          <img class="img-fluid rounded" style="width: 100%;" src="https://s3-us-west-1.amazonaws.com/fiteanalytics/website-assets/python.png" alt="">
-        </div>
-      </div>
-      <!-- /.row -->
-    </div>
-    <!-- /.container -->
-    <?php include 'footer.php'; ?>
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-      ga('create', 'UA-44140025-1', 'auto');
-      ga('require', 'displayfeatures');
-      ga('send', 'pageview');
-    </script>
-    <script type='application/ld+json'>
-    {
-      "@context": "http://www.schema.org",
-      "@type": "Corporation",
-      "name": "Fite Analytics LLC",
-      "founder": "Geoff Fite",
-      "url": "www.fiteanalytics.com",
-      "sameAs": [
-        "fiteanalytics.com"
-      ],
-      "description": "financial software and data",
-      "address": {
-        "@type": "PostalAddress",
-        "postOfficeBoxNumber": "",
-        "addressLocality": "MANHATTAN BEACH",
-        "addressRegion": "CA",
-        "postalCode": "90266-2558",
-        "addressCountry": "USA"
-      },
-      "contactPoint": {
-        "@type": "ContactPoint",
-        "telephone": "+14242620068",
-        "contactType": "customer support"
-      }
-    }
-    </script>
-  </body>
+        <!-- /.container -->
+        <?php include 'footer.php'; ?>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-44140025-1', 'auto');
+            ga('require', 'displayfeatures');
+            ga('send', 'pageview');
+        </script>
+        <script type='application/ld+json'>
+        {
+            "@context": "http://www.schema.org",
+            "@type": "Corporation",
+            "name": "Fite Analytics LLC",
+            "founder": "Geoff Fite",
+            "url": "www.fiteanalytics.com",
+            "sameAs": [
+            "fiteanalytics.com"
+            ],
+            "description": "financial software and data",
+            "address": {
+            "@type": "PostalAddress",
+            "postOfficeBoxNumber": "",
+            "addressLocality": "MANHATTAN BEACH",
+            "addressRegion": "CA",
+            "postalCode": "90266-2558",
+            "addressCountry": "USA"
+            },
+            "contactPoint": {
+            "@type": "ContactPoint",
+            "telephone": "+14242620068",
+            "contactType": "customer support"
+            }
+        }
+        </script>
+    </body>
 </html>
