826a19edacb655b743292311bf99a8e18715d4db
Geoff Fite <geoff@fiteanalytics.com>
2019-03-17 12:14:35 -0700

    gish-0001

--- index2.php
+++ index2.php
@@ -15,7 +15,7 @@
   <body>
     <?php include 'navigation.php'; ?>
     <div class="jumbotron"  style="background-image: url(https://s3-us-west-1.amazonaws.com/fiteanalytics/website-assets/bridge_skyward.jpeg); background-size: 100% 100%; opacity: 1.2">
-      <h1 style="color: --tangerine">TRANSPARENCY, EFFICIENCY & INSIGHT for<br>INSTITUTIONAL PORTFOLIOS</h1>
+      <h1 style="color: tangerine">TRANSPARENCY, EFFICIENCY & INSIGHT for<br>INSTITUTIONAL PORTFOLIOS</h1>
       <p class="lead">Analytics for the <strong>next generation</strong> in Capital Markets</p>
       <p><a href="">Learn more &raquo;</a></p>
     </div>
